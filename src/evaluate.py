58547d3b29e49a8914852e4d52dfa99026479b3d
hungsvdut2k2 <hungsvdut@gmail.com>
2023-10-03 14:16:45 +0700

    feat: implement evaluate metrics

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,22 +1,20 @@
 import evaluate
+from seqeval.metrics import classification_report
 import numpy as np
-from src.constants import NER_TAGS
 
-metric = evaluate.load("seqeval")
+clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
 
 
-def compute_metrics(eval_preds):
-    logits, labels = eval_preds
-    predictions = np.argmax(logits, axis=-1)
-    true_labels = [[NER_TAGS[l] for l in label if l != -100] for label in labels]
-    true_predictions = [
-        [NER_TAGS[p] for (p, l) in zip(prediction, label) if l != -100]
-        for prediction, label in zip(predictions, labels)
-    ]
-    all_metrics = metric.compute(predictions=true_predictions, references=true_labels)
-    return {
-        "precision": all_metrics["overall_precision"],
-        "recall": all_metrics["overall_recall"],
-        "f1": all_metrics["overall_f1"],
-        "accuracy": all_metrics["overall_accuracy"],
-    }
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+
+    y_true = labels.tolist()
+    y_pred = predictions.tolist()
+
+    y_true = [["B-" + str(label)] for label in y_true]
+    y_pred = [["B-" + str(label)] for label in y_pred]
+    metrics = classification_report(y_true, y_pred, digits=3)
+    print(metrics)
+
+    return clf_metrics.compute(predictions=predictions, references=labels)
